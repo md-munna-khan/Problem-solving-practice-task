ee7ad650483a7e98a1a008b5d671ac26b14b3729
md.munna <mk8761174@gmail.com>
2026-04-09 18:00:05 +0600

    practice day 1 and seconde day complete

---
+++ problem_2_5.c
@@ -0,0 +1,61 @@
+// Zero or Non Zero
+// #include<stdio.h>
+// int main ()
+// {
+//     int  n;
+//     scanf("%d",&n);
+//     if(n==0)
+//     {
+//         printf("Zero");
+//     }
+//     else
+//     {
+//          printf("Non Zero");
+//     }
+//     return 0;
+// }
+
+// 5 Marks for good hand writing
+
+// #include<stdio.h>
+// int main ()
+// {
+//     int  n;
+//     scanf("%d",&n);
+//    int a = 5;
+//    int sum = n+a;
+//    printf("%d",sum);
+//     return 0;
+// }
+
+// Multiple or not
+
+// #include<stdio.h>
+// int main ()
+// {
+//     int a;
+//     int b;
+//     scanf("%d",&a);
+//     scanf("%d",&b);
+// if(a%b==0)
+// {
+//     printf("Yes");
+
+// } else
+// {
+//      printf("No");
+// }
+   
+// }
+
+// Floating Point Number
+
+
+#include<stdio.h>
+int main ()
+{
+    float a;
+    scanf("%f",&a);
+printf("%.3f",a);
+   
+}
---
+++ problem_3_5.c
@@ -0,0 +1,69 @@
+// I Love Practice
+// #include<stdio.h>
+// int main ()
+// {
+//     printf("I Love Practice");
+// }
+
+// Sum of Two Numbers
+// #include<stdio.h>
+// int main ()
+// {
+//    int a;
+//    int b;
+//    scanf("%d",&a);
+//    scanf("%d",&b);
+//    int sum = a+b;
+//    printf("%d\n",sum);
+// }
+
+// Variables
+
+
+// #include<stdio.h>
+// int main ()
+// {
+//    int A;
+//    long long int B;
+//    float C;
+//    char D;
+//    scanf("%d",&A);
+//    scanf("%lld",&B);
+//    scanf("%f",&C);
+//    scanf(" %c",&D);
+//    printf("%d\n",A);
+//    printf("%lld\n",B);
+//    printf("%.2f\n",C);
+// printf("%c\n",D); 
+// }
+
+// N Times
+
+// #include<stdio.h>
+// int main ()
+// {
+//   int N;
+//   scanf("%d",&N);
+//   for (int i =0;i<N;i++){
+//     printf("I Love Practice\n");
+//   }
+ 
+// }
+
+// Divisible By 5 or Not
+
+#include<stdio.h>
+int main ()
+{
+  int N;
+  scanf("%d",&N);
+  for (int i =1;i<=N;i++){
+  if(i%5==0){
+    printf("%d Yes\n",i);
+  }
+   else{
+  printf("%d No\n",i);
+  }
+  }
+
+}
